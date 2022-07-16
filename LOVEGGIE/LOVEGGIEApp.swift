4ac4b55f287a72b99e0ac3e2c36e89af5113756d
Joe <joekojima3@gmail.com>
2022-07-16 23:01:22 +0900

    Initial Commit

---
+++ LOVEGGIE/LOVEGGIEApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOVEGGIEApp.swift
+//  LOVEGGIE
+//
+//  Created by Joe Kojima on 7/16/22.
+//
+
+import SwiftUI
+
+@main
+struct LOVEGGIEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
