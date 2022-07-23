77c9a41d7a37ad565ec326d6186d1347ea9f4600
Joe <joekojima3@gmail.com>
2022-07-23 22:24:51 +0900

    Added user upload history on upload tab

--- LOVEGGIE/ContentView.swift
+++ LOVEGGIE/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import FirebaseStorage
+import FirebaseFirestore
 
 struct ContentView: View {
     var body: some View {
@@ -62,6 +63,7 @@ struct FirstView: View
         ZStack
         {
             Color.white.ignoresSafeArea(edges: .top)
+            
         }
     }
 }
@@ -81,6 +83,7 @@ struct ThirdView: View
 {
     @State var isPickerShowing = false
     @State var selectedImage: UIImage?
+    @State var retrievedImages = [UIImage]()
     var body: some View
     {
         ZStack
@@ -110,12 +113,27 @@ struct ThirdView: View
                     } label: {
                         Text("Upload Photo")
                     }
+                    
+                }
+                
+                Divider()
+                
+                HStack {
+                    //loop through images
+                    ForEach(retrievedImages, id: \.self) { image in
+                        Image(uiImage: image)
+                            .resizable()
+                            .frame(width: 100, height: 100)
+                    }
                 }
             }
              .sheet(isPresented: $isPickerShowing, onDismiss: nil) {
                 //Image picker
                 ImagePicker(selectedImage: $selectedImage, isPickerShowing: $isPickerShowing)
             }
+             .onAppear {
+                 retrievePhotos()
+             }
         }
     }
     
@@ -128,7 +146,6 @@ struct ThirdView: View
         // Create storage reference
         let storageRef = Storage.storage().reference()
         
-        
         // turn image to data
         let imageData = selectedImage!.jpegData(compressionQuality: 0.0)
         guard imageData != nil else{
@@ -136,19 +153,79 @@ struct ThirdView: View
         }
         
         // specify file path and name
-        let fileRef = storageRef.child("images/\(UUID().uuidString).jpg")
+        let path = "images/\(UUID().uuidString).jpg"
+        let fileRef = storageRef.child(path)
         
         // Upload data
         let uploadTask = fileRef.putData(imageData!, metadata: nil) { metadata, error in
             // check error
             if error == nil && metadata != nil {
-                
+                // save reference to the file in firestore database
+                let db = Firestore.firestore()
+                db.collection("images").document().setData(["url":path]) { error in
+                    // if there are no error display the new image
+                    if error == nil {
+                        DispatchQueue.main.async {
+                            self.retrievedImages.append(self.selectedImage!)
+                        }
+                    }
+                }
             }
             
         }
-        
-        // Save referecne to that data
+        // Save reference to that data
     }
+    
+    func retrievePhotos() {
+        
+        // Get data from database
+        let db = Firestore.firestore()
+        
+        db.collection("images").getDocuments { snapshot, error in
+            if error == nil && snapshot != nil {
+                
+                var paths = [String]()
+                
+                //Loops through all the returned images
+                for doc in snapshot!.documents {
+                    
+                    //Extract the file path and add to array
+                    paths.append(doc["url"] as! String)
+                    
+                }
+                
+                //Loops throguh file paths and fetches the data form firebase storage
+                for path in paths {
+                    
+                    //Gets a reference to storage
+                    let storageRef = Storage.storage().reference()
+                    
+                    //specifies path
+                    let fileRef = storageRef.child(path)
+                    
+                    //retrieve the data
+                    fileRef.getData(maxSize: 5 * 1024 * 1024) { data, error in
+                        
+                        //check error
+                        if error == nil && data != nil {
+                            //create ui image and insert to array
+                            if let image = UIImage(data: data!) {
+                                
+                                DispatchQueue.main.async {
+                                    retrievedImages.append(image)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        // Display image
+        
+    }
+    
+    
 }
     
 struct FourthView: View
