aad79aaf9de6bbe8bfa5893e56768ecc178031de
Joe <joekojima3@gmail.com>
2022-07-25 09:32:43 +0900

    added buy select item number

--- LOVEGGIE/View/First View/BuyView.swift
+++ LOVEGGIE/View/First View/BuyView.swift
@@ -10,18 +10,47 @@ import SwiftUI
 
 struct BuyView: View
 {
+    @State private var isExpanded = false
+    @State private var selectedNum = 1
     var body: some View
     {
         ZStack
         {
             VStack {
+                Text("Number of Items")
+                    .font(.title3)
+                DisclosureGroup("\(selectedNum)", isExpanded: $isExpanded) {
+                    VStack {
+                        ForEach(1...9, id: \.self) { num in
+                            Text("\(num)")
+                                .font(.title3)
+                                .padding(.all)
+                                .onTapGesture {
+                                    self.selectedNum = num
+                                    withAnimation{
+                                        self.isExpanded.toggle()
+                                    }
+                                }
+                        }
+                    }
+                }
+                .accentColor(.white)
+                .font(.title2)
+                .foregroundColor(.white)
+                .padding(.all)
+                .background(Color.blue)
+                .cornerRadius(8)
+                
+                
+                Divider()
+                
                 Button {
                     
                 } label: {
                     Text("Buy")
                 }
                 .buttonStyle(.borderedProminent)
-            }
+            }.padding(.all)
         }
     }
 }
